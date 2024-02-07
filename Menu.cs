2dc631451c7eab1d294c0657aa096d11a2b926e7
RatoufRid <ridah.raouf@gmail.com>
2024-02-07 08:35:54 +0100

    [+] Fix path logs and add to menu

--- Menu.cs
+++ Menu.cs
@@ -16,6 +16,13 @@ namespace EasySave_v1
             string SourceDirectory = "";
             string Type = "";
 
+            //Total Backup
+            string backupCount = "";
+            int backupCountInt = 0;
+            int NumberBackup = 0;
+
+            List<Tuple<int, string, string, string, string>> backupInfoList = new List<Tuple<int, string, string, string, string>>();
+
             Backup FirstBackup = new Backup(Name, SourceDirectory, TargetDirectory);
             Log.Information("Application started");
 
@@ -24,54 +31,76 @@ namespace EasySave_v1
                 Console.WriteLine("-----------------");
                 Console.WriteLine("| 1 -- Backup   |");
                 Console.WriteLine("-----------------");
-                Console.WriteLine("| 2 -- Modify   |");
-                Console.WriteLine("-----------------");
-                Console.WriteLine("| 3 -- Delete   |");
-                Console.WriteLine("-----------------");
-                Console.WriteLine("| 4 -- Quit     |");
+                Console.WriteLine("| 2 -- Quit     |");
                 Console.WriteLine("-----------------");
                 Console.WriteLine("\nEnter your choose : ");
                 string Choose = Console.ReadLine();
                 switch (Choose)
                 {
-                    //Create a TargetRepertory on local machine
-                    case "Backup":
-                        try
+                    //Backup
+                    case "1":
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine("| 1 -- Create   |");
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine("| 2 -- Select   |");
+                        Console.WriteLine("-----------------");
+                        Console.WriteLine("\nEnter your choose : ");
+                        Choose = Console.ReadLine();
+                        switch (Choose)
                         {
-                            Console.WriteLine("Enter Target Directory :");
-                            TargetDirectory = Console.ReadLine();
-                            Console.WriteLine("Enter Directory Name :");
-                            Name = Console.ReadLine();
-                            TargetDirectory = TargetDirectory + @"\" + Name;
-                            FirstBackup.CreateTargetDirectory(TargetDirectory);
-                            Console.WriteLine("Repository has been created successfully!");
-                            Log.Information("Repository has been created successfully!");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Error creating target directory : {ex.Message}");
-                            Log.Error(ex, "Error creating target directory");
+                            case "1":
+                                Console.WriteLine("Enter this amount of back-up");
+                                backupCount = Console.ReadLine();
+                                backupCountInt = int.Parse(backupCount);
+
+                                if (backupCountInt >= 1 & backupCountInt <= 5)
+                                {
+                                    while (NumberBackup != backupCountInt)
+                                    {
+                                        //Donner le nom du répertoire cible
+                                        Console.WriteLine("Enter Directory Name :");
+                                        Name = Console.ReadLine();
+
+                                        //Selectionner le chemin répertoire source
+                                        Console.WriteLine("Enter Source Directory :");
+                                        SourceDirectory = Console.ReadLine();
+
+                                        //Selectionner le chemin répertoire cible
+                                        Console.WriteLine("Enter Target Directory :");
+                                        TargetDirectory = Console.ReadLine();
+
+                                        //Selectionner le type de back-up
+                                        Console.WriteLine("Enter Type Backup (Full/Diff)");
+                                        Type = Console.ReadLine();
+
+                                        //Combiner le chemin avec le nom
+                                        TargetDirectory = TargetDirectory + @"\" + Name;
+                                        Console.WriteLine("Repository has been created successfully!");
+
+                                        NumberBackup++; // Incrémenter le compteur de sauvegardes créées
+
+                                        // Store the values in the list
+                                        backupInfoList.Add(new Tuple<int, string, string, string, string>(NumberBackup, Name, SourceDirectory, TargetDirectory, Type)); ; ;
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Erreur!");
+                                }
+                                break;
+
+                            case "2":
+                                // Access the stored values
+                                foreach (var backupInfo in backupInfoList)
+                                {
+                                    Console.WriteLine($"Backup: {backupInfo.Item1} Name: {backupInfo.Item2}, Source Directory: {backupInfo.Item3}, Target Directory: {backupInfo.Item4}, Type: {backupInfo.Item5}");
+                                }
+                                break;
                         }
                         break;
-                    //Backup a source directory into target directory 
-                    case "Modify":
-                        try
-                        {
-                            Console.WriteLine("\nEnter Source Directory :");
-                            SourceDirectory = Console.ReadLine();
-                            FirstBackup.CopyRepertory(TargetDirectory, SourceDirectory);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Error backup : {ex.Message}");
-                            Log.Error(ex, "Error backup");
-                        }
-                        break;
-                    case "Delete":
-                        FirstBackup.CompareElement(TargetDirectory, SourceDirectory, "Complet");
-                        break;
-                    case "Quit":
-                        Log.Information("Close the application succesfully");
+                    //Quit
+                    case "2":
                         Environment.Exit(0);
                         break;
 
